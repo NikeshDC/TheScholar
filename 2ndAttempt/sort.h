a7330b2bbae5c1c76421c36b66512a05ddca3ba4
NikeshDC <60224139+NikeshDC@users.noreply.github.com>
2020-11-27 20:22:05 +0545

    added heapsort

--- 2ndAttempt/sort.h
+++ 2ndAttempt/sort.h
@@ -103,6 +103,106 @@ public:
         }
     }
 
+
+class HeapSort
+{
+    std::fstream &sortfile;
+    int basepos;
+    int noOfEls;
+    int ELSIZE;
+    char DELIM;
+    std::string str1;
+    std::string str2;
+public:
+    HeapSort(std::fstream& _file,int _noOfEls, int _ELSIZE, char _DELIM = ' '):sortfile(_file)
+    {
+        basepos = _file.tellg();
+        noOfEls = _noOfEls;
+        ELSIZE = _ELSIZE;
+        DELIM = _DELIM;
+    }
+    HeapSort(std::fstream& _file, int _ELSIZE, char _DELIM = ' '):sortfile(_file)
+    {
+        basepos = _file.tellg();
+        _file.seekg(0, std::ios::end);
+        int endpos = _file.tellg();
+        noOfEls = (endpos - basepos)/_ELSIZE;
+        _file.seekg(basepos);
+        ELSIZE = _ELSIZE;
+        DELIM = _DELIM;
+    }
+private:
+    void makeheap();
+    void sortFromHeap();
+public:
+    void sort()
+    {
+        makeheap();
+        sortFromHeap();
+    }
+};
+void HeapSort::makeheap()
+{
+    int q;
+    for(int i=1; i<noOfEls; i++)
+        for(int j=i; j>=0;j = q)
+        {
+            q = (j-1)/2;
+            getString(sortfile, str1, ELSIZE, j, basepos);
+            getString(sortfile, str2, ELSIZE, q, basepos);
+            //COMPARING WITH FATHER NODE
+            if(str2<str1)
+            {
+                //SWAPPING FATHER AND CHILD
+                putString(sortfile, str1, ELSIZE, q, basepos);
+                putString(sortfile, str2, ELSIZE, j, basepos);
+            }
+            else
+                break;
+        }
+}
+void HeapSort::sortFromHeap()
+{
+    int r,l;
+    std::string a;
+    for(int i=noOfEls-1; i>0; i--)
+     {
+        //SWAPPING LARGEST ELEMENT FROM INDEX 0 TO PROPER POSITION
+        getString(sortfile, str1, ELSIZE, 0, basepos);
+        getString(sortfile, str2, ELSIZE, i, basepos);
+        putString(sortfile, str1, ELSIZE, i, basepos);
+        putString(sortfile, str2, ELSIZE, 0, basepos);
+        //HEAPIFY THE REMAINING HEAP
+        for(int j=0; 2*j+2<i;)
+        {
+            l = 2*j+1;
+            r = 2*j+2;
+            getString(sortfile, a, ELSIZE, j, basepos);
+            getString(sortfile, str1, ELSIZE, l, basepos);
+            getString(sortfile, str2, ELSIZE, r, basepos);
+
+            if(a>= str1 && a>= str2)//FATHER NODE IS ALREADY LARGEST
+                break;
+            else
+            {
+                if(str1 >= str2)
+                {
+                    putString(sortfile, str1, ELSIZE, j, basepos);
+                    putString(sortfile, a, ELSIZE, l, basepos);
+                    j = l;
+                }
+                else
+                {
+                    putString(sortfile, str2, ELSIZE, j, basepos);
+                    putString(sortfile, a, ELSIZE, r, basepos);
+                    j = r;
+                }
+            }
+        }
+     }
+}
+
+
 void insertInSortedFile(std::fstream& sfile, const std::string &insert_String, const int noOfEls, const int ELSIZE, const char DELIM = ' ')
 {
     int basepos = sfile.tellg();
